e14226bdd0680286d385e2f39eef3a98d8e84570
Rupak Vignesh Swaminathan <rupakvigneshswaminathan@Rupaks-Mac-mini.local>
2025-11-22 22:35:56 -0500

    micrograd skeleton

---
+++ rmicrograd/src/engine.py
@@ -0,0 +1,22 @@
+class Value:
+    def __init__(self):
+        pass
+    
+    def __add__(self):
+        pass
+    
+    def __mul__(self):
+        pass 
+
+    def __pow__(self):
+        pass 
+
+    def relu(self):
+        pass
+
+    def backward(self):
+        pass
+  
+    def __repr(self):
+        pass
+     
---
+++ rmicrograd/src/nn.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+from rmicrograd.src.engine import Value
+
+class Module:
+    def zero_grad(self):
+        pass
+
+    def parameters(self):
+        pass
+
+
+class Neuron(Module):
+
+
+class Layer(Module):
+
+
+class MLP(Module):
+
+
+ 
